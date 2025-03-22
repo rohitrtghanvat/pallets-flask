ef4851c4ef59be6435fa6737185bf2aaa8b5c004
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-03-22 14:40:17 +0530

    Saving local changes before pulling

--- flask_Hello.py
+++ flask_Hello.py
@@ -1,16 +1,4 @@
-# from flask import Flask
-
-# app = Flask(__name__)  # Initialize Flask app
-
-# @app.route("/")  # Define a route (URL)
-# def home():
-#     return "Hello, Flask!"  # Response when visiting "/"
-
-# if __name__ == "__main__":
-#     app.run(debug=True)  # Run the app
-
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
